452b1d384e088c755003ac70fed819f8fd348125
Chris <christianto.g.14@gmail.com>
2018-04-29 15:25:49 +0700

    removing login for karyawan

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('config/autoload.php');
-
-// proses login disini
-
-$error = null;
-
-// tampilkan halaman login.php (dari folder views).
-View::render("login.php", ["error"=>$error]);
